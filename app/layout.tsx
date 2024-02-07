d9ae5597c471dc608a239dc8970cc9a853c42755
manager <cbwstar@gmail.com>
2024-02-07 09:32:50 +0900

    CBW 대시보드

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | 대시보드',
+    default: 'CBW 대시보드',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 export default function RootLayout({
   children,
